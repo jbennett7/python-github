d0d34493e7796fc60b097a166fe0bc3bb1d55e79
Joseph Bennett <jbennett@epix.com>
2017-07-27 17:12:31 -0400

    working on github_repo.py

--- github_repo.py
+++ github_repo.py
@@ -30,7 +30,8 @@ class GithubRepository(object):
             self.exists = False
         elif result.status_code == 200:
             self.exists = True
-            self._repository = result.json()
+            self._configuration = dict((k, result.json()[k]) for k in [i in i, o in CONFIGURABLE_KEY_LIST])
+            self._labels = requests.get(re.sub('{/name}', '', result.json()['labels_url']), headers=header)
 
     def create_repository(self):
         assert self._configuration, "Repository, {}, has no configuration.".format(self._name)
@@ -48,15 +49,41 @@ class GithubRepository(object):
                 except AssertionError:
                     raise
         self._repository = result.json()
+        return result.status_code
 
     def set_configuration(self, configuration):
         assert isinstance(configuration, dict)
-        assert len(configuration.keys()) == len(CONFIGURABLE_KEY_LIST)
         for key in configuration:
             assert key in [k[0] for k in CONFIGURABLE_KEY_LIST], "key {} does not exist in {}".format(key, CONFIGURABLE_KEY_LIST)
             assert isinstance(configuration[key], [o for k, o in CONFIGURABLE_KEY_LIST if k == key][0])
         self._configuration = configuration
+        result = requests.get(self._url, self._header, data=self._configuration)
+        return result.status_code
 
     def get_configuration(self):
         assert self._configuration, "Repository, {}, has no configuration.".format(self._name)
         return self._configuration
+
+    def get_labels(self):
+        try:
+            return self._labels
+        except Exception:
+            try:
+                result = requests.get(self._url, headers=self._header)
+                if result.status_code != 200:
+                    raise Exception, "Repository, (), does not exist".format(self._name)
+                labels_url = re.sub('{/name}', '', result.json()['labels_url'])
+                result = requests.get(labels_url, headers=header)
+                self._labels = [dict((k,v[k]) for k in ['name', 'color']) for v in result.json()]
+            except Exception:
+                raise
+        return self.labels
+
+#   def stage_labels(self, label_list):
+#       assert isinstance(label_list, list)
+#       for label in label_list:
+#           assert
+
+
+
+
